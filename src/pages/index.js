778f30eda7738dc2fe074e36a7bc894b4d1a7fd8
SergeyKirintsev <kserg80@gmail.com>
2021-04-23 03:25:28 +0300

    feat: make JS for menu

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,19 @@
-'use strict';
+"use strict";
 
-import './index.css';
+import "./index.css";
+
+const menu = document.querySelector(".header__menu");
+const menuItems = document.querySelectorAll(".header__link");
+
+const menuLang = document.querySelector(".intro__lang");
+const menuLangItems = document.querySelectorAll(".intro__lang-link");
+
+menu.addEventListener("click", (evt) => {
+  menuItems.forEach((el) => el.classList.remove("header__link_active"));
+  evt.target.classList.add("header__link_active");
+});
+
+menuLang.addEventListener("click", (evt) => {
+  menuLangItems.forEach((el) => el.classList.remove("intro__lang-link_active"));
+  evt.target.classList.add("intro__lang-link_active");
+});
