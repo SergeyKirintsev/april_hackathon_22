74d68c7b5c852df8c2624ae8713bc1c672d00cd0
SergeyKirintsev <kserg80@gmail.com>
2021-04-24 04:24:35 +0300

    feat: scroll to headers

--- src/pages/index.js
+++ src/pages/index.js
@@ -106,7 +106,7 @@ for (let anchor of anchors) {
     evt.preventDefault(); // Предотвратить стандартное поведение ссылок
     // Атрибут href у ссылки, если его нет то перейти к body (наверх не плавно)
     const goto = anchor.hasAttribute("href") ? anchor.getAttribute("href") : "body";
-    // Плавная прокрутка до элемента с id = href у ссылки
+    // Плавная прокрутка до элемента с i d = href у ссылки
     const gotoBlock = document.querySelector(goto);
     const gotoBlockValue =
       gotoBlock.getBoundingClientRect().top +
